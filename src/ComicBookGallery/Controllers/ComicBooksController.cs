11342af3281077a2b60a33a0dc4b7729d587e6ab
kev1nster <calin.bogdan15@gmail.com>
2016-07-19 13:18:57 +0300

    Added Detail

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,7 +7,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    class ComicBookController : Controller
+    public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the Comic Books Controller!";
+        }   
     }
 }
