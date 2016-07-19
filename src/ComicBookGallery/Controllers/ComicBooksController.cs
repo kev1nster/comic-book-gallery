c955e37725f1e84518fa6cd5f075e81f93e610f1
kev1nster <calin.bogdan15@gmail.com>
2016-07-19 13:52:54 +0300

    Added Comic Book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,11 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from a Content Result PAGE");
+            return View();
         }   
     }
 }
